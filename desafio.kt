970734f4ae717178edb91c631111f1c3e6c3acfb
WillianTambori <95225636+WillianTambori@users.noreply.github.com>
2025-10-04 20:37:10 -0300

    completado o desafio da DIO.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,49 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
+// usuário que irá fazer a matricula terá nome e nivel de aprendizagem do curso
+// usuário que irá fazer a matricula terá nome e nivel de aprendizagem do curso
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// o curso terá disciplinas, a formação terá o papel de informar o nome do curso, disciplinas e inscritos, também a função de matricula do curso
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    // inscritos com uma lista MutableSet  que tem o papel de não armazenar inscritos repetidos.
+    val inscritos: MutableSet<Usuario> = mutableSetOf<Usuario>()
     
+    // matricula adiciona o usuario inscrito e informa se o usuário foi matriculado ou não.
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if(inscritos.add(usuario)) print("matriculados no curso $nome") else println("Usuario já matriculado")
+        println(inscritos)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    var linguagem: MutableList<ConteudoEducacional> = mutableListOf(
+        ConteudoEducacional("Python",60),
+        ConteudoEducacional("C#",120),
+        ConteudoEducacional("Kotlin",80),
+    	ConteudoEducacional("Php",90)
+        )
+    
+    var ti: Formacao = Formacao("Analise e desenvolvimento de software",linguagem)
+  
+    var adm: MutableList<ConteudoEducacional> = mutableListOf(
+        ConteudoEducacional("Contabilidade",60),
+        ConteudoEducacional("economia",120),
+        )
+
+    var administracao: Formacao = Formacao("Administração",adm)
+    
+    ti.matricular(Usuario("willian",Nivel.DIFICIL))
+    ti.matricular(Usuario("willian",Nivel.DIFICIL))// usuário repetido, e não será armazenado pois já foi realizado a sua matricula.
+    ti.matricular(Usuario("Rogerio",Nivel.BASICO))
+    ti.matricular(Usuario("fabricio",Nivel.INTERMEDIARIO))
+    administracao.matricular(Usuario("Melissa",Nivel.DIFICIL))
+    administracao.matricular(Usuario("Ronaldo",Nivel.BASICO))
+    
+           
+   
+    
 }
